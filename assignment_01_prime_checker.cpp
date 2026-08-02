b07c861afa04a679703111da4dbe9958f0d45449
Charles5611 <cakuffo37@gmail.com>
2026-08-02 23:19:45 +0000

    completed

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,37 +36,4 @@
 
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
-=======
-
->>>>>>> 1328c8153caa3692bf704b8afd4e855d00e2c712
-bool isPrime(int n);
-
-int main() {
-    int number;
-
-    cout << "Enter a number: ";
-    cin >> number;
-
-    if (isPrime(number)) {
-        cout << number << " is a prime number." << endl;
-    } else {
-        cout << number << " is NOT a prime number." << endl;
-    }
-
-    return 0;
-}
-
-bool isPrime(int n) {
-    if (n < 2) {
-        return false;
-    }
-
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
+<
